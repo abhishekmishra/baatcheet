4849bdc5ae0e55a630c29eda1798ff3487e1c211
Abhishek Mishra <abhishekmishra3@gmail.com>
2025-01-29 12:39:24 +0530

    stream response from the llm to the api

--- baatcheet.py
+++ baatcheet.py
@@ -1,5 +1,7 @@
 from fastapi import FastAPI
-from ollama import chat as ollama_chat
+from fastapi.responses import StreamingResponse
+from ollama import AsyncClient, ChatResponse
+import json
 
 app = FastAPI()
 
@@ -9,14 +11,20 @@ async def root():
     return {"message": "Hello World"}
 
 
-@app.get("/chat")
+async def ollama_chat(model, content, stream=True):
+    message = {"role": "user", "content": content}
+    async for part in await AsyncClient().chat(
+        model=model, messages=[message], stream=stream
+    ):
+        # print(part)
+        yield json.dumps(part.dict()).encode("utf-8")  # Convert part to dict and encode
+
+
+@app.get("/chat", response_model=ChatResponse)
 async def chat():
-    stream = ollama_chat(
-        model="deepseek-r1",
-        messages=[{"role": "user", "content": "Why is the sky blue?"}],
-        stream=True,
+    return StreamingResponse(
+        ollama_chat(
+            model="deepseek-r1:1.5b",
+            content="Why is the sky blue? Respond in one line. And don't overthink it.",
+        )
     )
-    response = ""
-    for chunk in stream:
-        response += chunk["message"]["content"]
-    return {"message": response}
